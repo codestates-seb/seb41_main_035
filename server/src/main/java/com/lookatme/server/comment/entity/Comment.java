2abc737556c4899871b2d94a7ef155fd570ee76f
hyejuc <chjjudy@naver.com>
2023-01-06 16:06:58 +0900

    feat: Comment 기본 entity 작성 완료

---
+++ server/src/main/java/com/lookatme/server/comment/entity/Comment.java
@@ -0,0 +1,52 @@
+package com.lookatme.server.comment.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "comments")
+public class Comment{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @CreatedDate
+    @Column(name = "created_date", updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "post_id")
+//    private Post post;
+    private Comment() {
+
+    }
+
+    @Builder
+    public Comment(final Long commentId,
+                   final String content) {
+        this.commentId = commentId;
+        this.content = content;
+    }
+
+    public void changeContent(final String content) {
+        this.content = content;
+    }
+}
