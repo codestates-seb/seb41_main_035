51720f4bd9849e9aba16cc6203102abf16d26807
김나율 <111343290+nayul34@users.noreply.github.com>
2023-01-09 10:19:40 +0900

    Feat/fe/ header (#5)
    * ✨ feat:login전 header 구현
    
    * ✨ feat:login후 header 구현
    
    * ✨ feat: react icon 설치

---
+++ client/src/components/LoginHeader.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+const LoginHeader = () => {
+  return (
+    <>
+      <SWrapper>
+        <SHeader>
+          <p className="title">Look at me</p>
+          <form>
+            <input
+              className="header-bottom-search"
+              type="search"
+              placeholder="브랜드명, 상품명으로 검색"
+            ></input>
+          </form>
+          <button className="login button">로그인</button>
+        </SHeader>
+      </SWrapper>
+    </>
+  );
+};
+const SWrapper = styled.div`
+  position: sticky;
+  top: 0;
+  z-index: 5;
+  justify-content: center;
+  display: flex;
+`;
+const SHeader = styled.div`
+  box-sizing: border-box;
+  background-color: #d9d9d9;
+  width: 70%;
+  height: 18vh;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  .title {
+    font-size: 60px;
+    margin-left: -45px;
+  }
+  input {
+    width: 35vw;
+    height: 5vh;
+    border: 2px solid black;
+    margin-top: 15px;
+    margin-left: -70px;
+    background: transparent;
+  }
+  button {
+    width: 5vw;
+    height: 30px;
+    margin-left: -50px;
+    margin-top: 15px;
+    background-color: #f67b7b;
+    color: white;
+  }
+`;
+
+export default LoginHeader;
