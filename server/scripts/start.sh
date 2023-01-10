9f812ae4e509f044264ba0fbdf5c910b3a1c5137
eheh12321 <eheh12321@gmail.com>
2023-01-10 19:20:30 +0900

    build: 무중단 배포 테스트 3
    - 거의 다 된듯

--- server/scripts/start.sh
+++ server/scripts/start.sh
@@ -23,6 +23,6 @@ IDLE_PROFILE=$(find_idle_profile) # SubShell을 호출해 함수의 결과값을
 echo "> ${JAR_NAME} 을 profile=${IDLE_PROFILE} 로 실행합니다"
 
 nohup java -jar \
-        -Dspring.config.location=/home/ec2-user/lookatme/server/application-dev.yml \
+        -Dspring.config.location=classpath:/application.yml,/home/ec2-user/lookatme/server/application-dev.yml \
         -Dspring.profiles.active=dev,${IDLE_PROFILE} \
         ${JAR_NAME} > ${REPOSITORY}/nohup.out 2>&1 &
