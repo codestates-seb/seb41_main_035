e76d1332729ac987a2035f7a6dd931e3f312bea8
eheh12321 <eheh12321@gmail.com>
2023-01-10 20:30:27 +0900

    build: 무중단 배포 테스트 4
    - 경로 수정

--- server/scripts/switch.sh
+++ server/scripts/switch.sh
@@ -10,7 +10,7 @@ function switch_proxy() {
   echo "> 전환할 Port: $IDLE_PORT"
   echo "> Port 전환"
   # Nginx가 변경할 프록시 주소 생성해서 파이프라인으로 넘김 | 덮어씌우기
-  echo "set \$service_url https://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+  echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
   echo "> Nginx Reload"
   sudo service nginx reload # restart(끊김 O) != reload(끊김 X)
 }
