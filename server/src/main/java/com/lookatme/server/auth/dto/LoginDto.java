63bcbe89938c73343c0e10db7fc8aee739ecee96
eheh12321 <eheh12321@gmail.com>
2023-01-12 10:52:17 +0900

    feat: Authcontroller 테스트 코드 작성

--- server/src/main/java/com/lookatme/server/auth/dto/LoginDto.java
+++ server/src/main/java/com/lookatme/server/auth/dto/LoginDto.java
@@ -1,8 +1,12 @@
 package com.lookatme.server.auth.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginDto {
     private String email;
     private String password;
