efde413374eea2a0f01dbbc76dd015f074a4d121
hyejuc <chjjudy@naver.com>
2023-01-12 19:03:59 +0900

    feat: commentControllerTest 멤버 관련 부분 추가

--- server/src/test/java/com/lookatme/server/comment/CommentControllerTest.java
+++ server/src/test/java/com/lookatme/server/comment/CommentControllerTest.java
@@ -1,6 +1,7 @@
 package com.lookatme.server.comment;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lookatme.server.auth.dto.MemberPrincipal;
 import com.lookatme.server.auth.jwt.JwtTokenizer;
 import com.lookatme.server.auth.jwt.RedisRepository;
 import com.lookatme.server.auth.utils.MemberAuthorityUtils;
@@ -10,6 +11,7 @@ import com.lookatme.server.comment.dto.CommentPostDto;
 import com.lookatme.server.comment.entity.Comment;
 import com.lookatme.server.comment.service.CommentService;
 import com.lookatme.server.config.SecurityConfiguration;
+import com.lookatme.server.member.entity.Member;
 import com.lookatme.server.member.service.MemberService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -17,13 +19,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.operation.preprocess.Preprocessors;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -42,19 +48,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
         MemberAuthorityUtils.class,
         JwtTokenizer.class
 })
-@WebMvcTest(CommentController.class)
+@WebMvcTest(controllers = CommentController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebMvcConfigurer.class)})
 @MockBean(JpaMetamodelMappingContext.class)
 @AutoConfigureRestDocs
 public class CommentControllerTest {
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private CommentService commentService;
-
     @MockBean
     private RedisRepository redisRepository;
-
     @MockBean
     private MemberService memberService;
 
@@ -74,13 +79,14 @@ public class CommentControllerTest {
                 .content(commentPostDto.getContent())
                 .build();
 
-        given(commentService.createComment(Mockito.any(CommentPostDto.class))).willReturn(comment);
+        given(commentService.createComment(Mockito.any(CommentPostDto.class), Mockito.any(MemberPrincipal.class))).willReturn(comment);
 
         //when
         ResultActions actions =
                 mockMvc.perform(post("/comment")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
                         .content(content)
                 );
         //then
@@ -88,6 +94,8 @@ public class CommentControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.commentId").value(comment.getCommentId()))
                 .andExpect(jsonPath("$.content").value(comment.getContent()))
+                .andExpect(jsonPath("$.nickname").value(comment.getMember().getNickname()))
+                .andExpect(jsonPath("$.profileImageUrl").value(comment.getMember().getProfileImageUrl()))
                 .andDo(document("create-comment",
                         Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
                         Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
@@ -101,7 +109,9 @@ public class CommentControllerTest {
                                         fieldWithPath("commentId").type(JsonFieldType.NUMBER).description("댓글 식별자"),
                                         fieldWithPath("content").type(JsonFieldType.STRING).description("댓글 내용"),
                                         fieldWithPath("createdDate").type(JsonFieldType.NULL).description("댓글 작성일"),
-                                        fieldWithPath("updatedDate").type(JsonFieldType.NULL).description("댓글 수정일")
+                                        fieldWithPath("updatedDate").type(JsonFieldType.NULL).description("댓글 수정일"),
+                                        fieldWithPath("nickname").type(JsonFieldType.STRING).description("댓글 작성자 닉네임"),
+                                        fieldWithPath("profileImageUrl").type(JsonFieldType.STRING).description("댓글 작성자 프로필 사진")
                                 )
                         )
                 ));
@@ -121,7 +131,18 @@ public class CommentControllerTest {
                 .content(commentPatchDto.getContent())
                 .build();
 
-        given(commentService.editComment(Mockito.anyLong(),Mockito.any(CommentPatchDto.class))).willReturn(comment);
+        Member member = Member.builder()
+                .memberId(1L)
+                .email("email@com")
+                .nickname("닉네임")
+                .profileImageUrl("http://프사링크")
+                .height(180)
+                .weight(70)
+                .build();
+
+        comment.addMember(member);
+
+        given(commentService.editComment(Mockito.anyLong(),Mockito.any(CommentPatchDto.class), Mockito.any(MemberPrincipal.class))).willReturn(comment);
 
         //when
         Long commentId = comment.getCommentId();
@@ -129,6 +150,7 @@ public class CommentControllerTest {
                 mockMvc.perform(patch("/comment/{commentId}", commentId)
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
                         .content(content)
                 );
         //then
@@ -136,6 +158,8 @@ public class CommentControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.commentId").value(comment.getCommentId()))
                 .andExpect(jsonPath("$.content").value(comment.getContent()))
+                .andExpect(jsonPath("$.nickname").value(comment.getMember().getNickname()))
+                .andExpect(jsonPath("$.profileImageUrl").value(comment.getMember().getProfileImageUrl()))
                 .andDo(document("patch-comment",
                         Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
                         Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
@@ -152,7 +176,9 @@ public class CommentControllerTest {
                                         fieldWithPath("commentId").type(JsonFieldType.NUMBER).description("댓글 식별자"),
                                         fieldWithPath("content").type(JsonFieldType.STRING).description("댓글 내용"),
                                         fieldWithPath("createdDate").type(JsonFieldType.NULL).description("댓글 작성일"),
-                                        fieldWithPath("updatedDate").type(JsonFieldType.NULL).description("댓글 수정일")
+                                        fieldWithPath("updatedDate").type(JsonFieldType.NULL).description("댓글 수정일"),
+                                        fieldWithPath("nickname").type(JsonFieldType.STRING).description("댓글 작성자 닉네임"),
+                                        fieldWithPath("profileImageUrl").type(JsonFieldType.STRING).description("댓글 작성자 프로필 사진")
                                 )
                         )
                 ));
@@ -167,6 +193,17 @@ public class CommentControllerTest {
                 .content("댓글 내용")
                 .build();
 
+        Member member = Member.builder()
+                .memberId(1L)
+                .email("email@com")
+                .nickname("닉네임")
+                .profileImageUrl("http://프사링크")
+                .height(180)
+                .weight(70)
+                .build();
+
+        comment.addMember(member);
+
         given(commentService.findComment(Mockito.anyLong())).willReturn(comment);
 
         //when
@@ -174,12 +211,15 @@ public class CommentControllerTest {
                 mockMvc.perform(get("/comment/{commentId}", comment.getCommentId())
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
                 );
         //then
         actions
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.commentId").value(comment.getCommentId()))
                 .andExpect(jsonPath("$.content").value(comment.getContent()))
+                .andExpect(jsonPath("$.nickname").value(comment.getMember().getNickname()))
+                .andExpect(jsonPath("$.profileImageUrl").value(comment.getMember().getProfileImageUrl()))
                 .andDo(document("get-comment",
                         Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
                         Preprocessors.preprocessResponse(Preprocessors.prettyPrint()),
@@ -191,7 +231,9 @@ public class CommentControllerTest {
                                         fieldWithPath("commentId").type(JsonFieldType.NUMBER).description("댓글 식별자"),
                                         fieldWithPath("content").type(JsonFieldType.STRING).description("댓글 내용"),
                                         fieldWithPath("createdDate").type(JsonFieldType.NULL).description("댓글 작성일"),
-                                        fieldWithPath("updatedDate").type(JsonFieldType.NULL).description("댓글 수정일")
+                                        fieldWithPath("updatedDate").type(JsonFieldType.NULL).description("댓글 수정일"),
+                                        fieldWithPath("nickname").type(JsonFieldType.STRING).description("댓글 작성자 닉네임"),
+                                        fieldWithPath("profileImageUrl").type(JsonFieldType.STRING).description("댓글 작성자 프로필 사진")
                                 )
                         )
                 ));
@@ -201,7 +243,7 @@ public class CommentControllerTest {
     public void deleteCommentTest() throws Exception {
         //given
         Long commentId = 1L;
-        doNothing().when(commentService).deleteComment(Mockito.anyLong());
+        doNothing().when(commentService).deleteComment(Mockito.anyLong(), Mockito.any(MemberPrincipal.class));
 
         //when
         ResultActions actions =
