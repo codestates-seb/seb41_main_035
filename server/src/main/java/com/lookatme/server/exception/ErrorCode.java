7b5db7276e2a69d9f41891fa16eb5dedbdc47fcd
eheh12321 <eheh12321@gmail.com>
2023-01-16 11:09:35 +0900

    refactor: fieldError 등 검증 에러 처리 추가

--- server/src/main/java/com/lookatme/server/exception/ErrorCode.java
+++ server/src/main/java/com/lookatme/server/exception/ErrorCode.java
@@ -9,7 +9,7 @@ public enum ErrorCode {
 
     // MEMBER
     MEMBER_NOT_FOUND("회원이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
-    MEMBER_EXISTS("이미 존재하는 회원입니다.", HttpStatus.BAD_REQUEST),
+    MEMBER_ACCOUNT_EXISTS("이미 존재하는 계정입니다.", HttpStatus.BAD_REQUEST),
     MEMBER_NICKNAME_EXISTS("이미 존재하는 닉네임입니다.", HttpStatus.BAD_REQUEST),
     MEMBER_ALREADY_FOLLOW("이미 팔로우 했습니다", HttpStatus.BAD_REQUEST),
     MEMBER_SELF_FOLLOW("자기 자신을 팔로우 할 수 없습니다", HttpStatus.BAD_REQUEST),
@@ -22,6 +22,7 @@ public enum ErrorCode {
     AUTHENTICATION_FAILED("검증에 실패했습니다.(액세스 토큰을 보냈는지 확인해주세요)", HttpStatus.FORBIDDEN),
     UNAUTHORIZED("접근 권한이 없습니다.", HttpStatus.FORBIDDEN),
     EXCEPTION("알 수 없는 에러가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    BAD_REQUEST("잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
     LOGIN_FAILED("이메일 혹은 비밀번호가 틀렸습니다", HttpStatus.BAD_REQUEST);
 
     private final String value;
