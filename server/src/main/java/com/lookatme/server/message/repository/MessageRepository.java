70482dfb590330de7142a8d0178f02105a867cdc
hyejuc <chjjudy@naver.com>
2023-01-17 13:00:21 +0900

    feat: 특정한 사람과 주고 받은 모든 메시지 조회 기능 구현

--- server/src/main/java/com/lookatme/server/message/repository/MessageRepository.java
+++ server/src/main/java/com/lookatme/server/message/repository/MessageRepository.java
@@ -9,9 +9,8 @@ import org.springframework.data.repository.query.Param;
 
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    @Query("select m from Message m where receiver_id = :id")
-    Page<Message> findAllMessagesByReceiver(@Param("id") Long receiverId, Pageable pageable);
-
-    @Query("select m from Message m where sender_id = :id")
-    Page<Message> findAllMessagesBySender(@Param("id") Long senderId, Pageable pageable);
+    @Query("select m from Message m where (sender_id = :sender_id and receiver_id = :receiver_id) or (sender_id = :receiver_id and receiver_id = :sender_id)")
+    Page<Message> findAllMessages(@Param("sender_id") Long senderId, @Param("receiver_id") Long receiverId, Pageable pageable);
+//    @Query("select m from Message m where sender_id = :id")
+//    Page<Message> findAllMessagesBySender(@Param("id") Long senderId, Pageable pageable);
 }
