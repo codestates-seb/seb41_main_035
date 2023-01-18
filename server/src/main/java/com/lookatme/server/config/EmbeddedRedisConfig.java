d229645bf449d79d3de9fdcbfdb228802c489d8c
eheh12321 <eheh12321@gmail.com>
2023-01-18 16:26:06 +0900

    refactor: Embedded Redis 실행 포트 선택 방법 수정

--- server/src/main/java/com/lookatme/server/config/EmbeddedRedisConfig.java
+++ server/src/main/java/com/lookatme/server/config/EmbeddedRedisConfig.java
@@ -2,7 +2,7 @@ package com.lookatme.server.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.util.StringUtils;
@@ -20,29 +20,68 @@ import java.io.InputStreamReader;
 @Configuration
 public class EmbeddedRedisConfig {
 
-    private final RedisProperties redisProperties;
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
     private RedisServer redisServer;
 
     @PostConstruct
     public void redisServer() throws IOException {
+
+        redisPort = isRedisRunning() ? findAvailablePort() : redisPort;
+
         redisServer = RedisServer.builder()
-                        .port(redisProperties.getPort())
+                        .port(redisPort)
                         .setting("maxmemory 128M")
                         .build();
-        try {
-            log.info(">>>>>>>>>>>> Redis 실행");
-            redisServer.start();
-        } catch (Exception e) {
-            log.info(">>>>>>>>>>>> 기존 Redis 사용");
-        }
+
+        log.info(">>>>>>>>>>>> {} 포트에서 Redis 실행", redisPort);
+        redisServer.start();
     }
 
     @PreDestroy
     public void stopRedis() {
         if (redisServer != null) {
-            log.info(">>>>>>>>>>>> Redis 종료");
+            log.info(">>>>>>>>>>>> {} 포트에서 Redis 종료", redisPort);
             redisServer.stop();
         }
+    }
+
+    /**
+     * Embedded Redis가 현재 실행중인지 확인
+     */
+    private boolean isRedisRunning() throws IOException {
+        return isRunning(executeGrepProcessCommand(redisPort));
+    }
+
+    /**
+     * 현재 PC/서버에서 사용가능한 포트 조회
+     */
+    public int findAvailablePort() throws IOException {
+        for (int port = 10000; port <= 65535; port++) {
+            Process process = executeGrepProcessCommand(port);
+            if (!isRunning(process)) {
+                return port;
+            }
+        }
+        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
+    }
+
+    /**
+     * 해당 port를 사용중인 프로세스 확인하는 sh 실행
+     */
+    private Process executeGrepProcessCommand(int port) throws IOException {
+        Process process = null;
+        try {
+            String command = String.format("netstat -nat | grep LISTEN | grep %d", port); // Linux
+            String[] shell = {"/bin/sh", "-c", command};
+            process = Runtime.getRuntime().exec(shell);
+        } catch (IOException e) {
+            String command = String.format("netstat -nao | find \"LISTEN\" | find \"%d\"", port); // Windows
+            String[] shell = {"cmd.exe", "/y", "/c", command};
+            process = Runtime.getRuntime().exec(shell);
+        }
+        return process;
     }
 
     /**
@@ -53,14 +92,10 @@ public class EmbeddedRedisConfig {
         StringBuilder pidInfo = new StringBuilder();
 
         try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-
             while ((line = input.readLine()) != null) {
                 pidInfo.append(line);
             }
-
-        } catch (Exception e) {
-        }
-
-        return !StringUtils.hasText(pidInfo.toString());
+        } catch (Exception e) {}
+        return StringUtils.hasText(pidInfo.toString());
     }
 }
