e8f7e1ec405cbfa35b71f3567b8455d0e201214a
hyejuc <107419210+hyejuc@users.noreply.github.com>
2023-01-19 11:08:48 +0900

    Feat/be/message (#55)
    * feat: 특정한 사람과 주고 받은 모든 메시지 조회 기능 구현
    
    * fix: MessageControllerTest 주석 처리
    
    * refactor: getMessages 코드 리팩토링
    
    * feat: getMessages 테스트 코드 작성
    
    * style: MessageService 코드 리팩토링

--- server/src/main/java/com/lookatme/server/message/controller/MessageController.java
+++ server/src/main/java/com/lookatme/server/message/controller/MessageController.java
@@ -41,25 +41,8 @@ public class MessageController {
                                                                @PathVariable("memberId") final Long memberId,
                                                                @RequestParam("page") final int page,
                                                                @RequestParam("size") final int size) {
-        Page<Message> messages = messageService.getMessages(memberPrincipal, memberId, page - 1, size);
-        List<MessageResponseDto> messageResponseDtos = messages.getContent()
-                .stream()
-                .map(message -> MessageResponseDto.of(message))
-                .collect(Collectors.toList());
-        return new ResponseEntity<>(new MultiResponseDto<>(messageResponseDtos, messages), HttpStatus.OK);
+        return new ResponseEntity<>(messageService.getMessages(memberPrincipal, memberId, page - 1, size), HttpStatus.OK);
     }
-
-//    @GetMapping("/sent")
-//    public ResponseEntity<MultiResponseDto> getSentMessage(@AuthenticationPrincipal MemberPrincipal memberPrincipal,
-//                                         @RequestParam("page") final int page,
-//                                         @RequestParam("size") final int size) {
-//        Page<Message> messages = messageService.getSentMessage(memberPrincipal, page - 1, size);
-//        List<MessageResponseDto> messageResponseDtos = messages.getContent()
-//                .stream()
-//                .map(message -> MessageResponseDto.of(message))
-//                .collect(Collectors.toList());
-//        return new ResponseEntity<>(new MultiResponseDto<>(messageResponseDtos, messages), HttpStatus.OK);
-//    }
 
     @DeleteMapping("/received/{messageId}")
     public ResponseEntity deleteReceivedMessage(@PathVariable("messageId") Long messageId,
