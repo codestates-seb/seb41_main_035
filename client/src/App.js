c35006885c0d263e04decc180de634408970ccc8
김나율 <dagg762@gmail.com>
2023-01-20 02:45:54 +0900

    ♻️ refactor: category useEffect로 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,28 @@
 // import React from 'react';
 import './App.css';
 import LoginHeader from './components/LoginHeader';
-import Outer from './pages/Outer';
-import Top from './pages/Top';
-import Bottom from './pages/Bottom';
-import Onepiece from './pages/Onepiece';
-import Hat from './pages/Hat';
-import Shoes from './pages/Shoes';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Footer from './components/Footer';
 import PostUpload from './pages/PostUpload';
 import PostView from './pages/PostView';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Category from './pages/Category';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <LoginHeader />
+        <Sidebar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile/:userId" element={<Profile />} />
           <Route path="/post/" element={<PostUpload />} />
-          <Route path="/outer" element={<Outer />} />
-          <Route path="/top" element={<Top />} />
-          <Route path="/bottom" element={<Bottom />} />
-          <Route path="/onepiece" element={<Onepiece />} />
-          <Route path="/hat" element={<Hat />} />
-          <Route path="/shoes" element={<Shoes />} />
           <Route path="/postupload" element={<PostUpload />} />
           <Route path="/postview" element={<PostView />} />
+          <Route path="/category/:categoryId" element={<Category />} />
         </Routes>
         <Footer />
       </BrowserRouter>
