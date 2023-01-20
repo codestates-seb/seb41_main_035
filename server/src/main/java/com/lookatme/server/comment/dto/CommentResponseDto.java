064ee662f9085ccfedfd0f4d63a1c731e38093e5
hyejuc <chjjudy@naver.com>
2023-01-20 22:56:07 +0900

    feat: Comment response에 Board 정보 추가

--- server/src/main/java/com/lookatme/server/comment/dto/CommentResponseDto.java
+++ server/src/main/java/com/lookatme/server/comment/dto/CommentResponseDto.java
@@ -1,5 +1,7 @@
 package com.lookatme.server.comment.dto;
 
+import com.lookatme.server.board.dto.BoardResponseDto;
+import com.lookatme.server.board.entity.Board;
 import com.lookatme.server.comment.entity.Comment;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +16,11 @@ public class CommentResponseDto {
     private LocalDateTime updatedDate;
     private String nickname;
     private String profileImageUrl;
+    private BoardResponseDto board;
+
+    private CommentResponseDto() {
+
+    }
 
     @Builder
     public CommentResponseDto(final Long commentId,
@@ -21,13 +28,15 @@ public class CommentResponseDto {
                               final LocalDateTime createdDate,
                               final LocalDateTime updatedDate,
                               final String nickname,
-                              final String profileImageUrl) {
+                              final String profileImageUrl,
+                              final BoardResponseDto board) {
         this.commentId = commentId;
         this.content = content;
         this.createdDate = createdDate;
         this.updatedDate = updatedDate;
         this.nickname = nickname;
         this.profileImageUrl = profileImageUrl;
+        this.board = board;
     }
 
     public static CommentResponseDto of(final Comment comment) {
@@ -38,6 +47,7 @@ public class CommentResponseDto {
                 .updatedDate(comment.getUpdatedDate())
                 .nickname(comment.getMember().getNickname())
                 .profileImageUrl(comment.getMember().getProfileImageUrl())
+                .board(BoardResponseDto.of(comment.getBoard()))
                 .build();
     }
 }
